ef9b34128d850993f2a9ca0f00e6384cd4de97bb
Anastasiya Lopatina <anastasiya.lopatina@simbirsoft.com>
2017-02-17 13:36:10 +0400

    Added some changes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Chart from  './Chart.js'
 
 class App extends Component {
+
   render() {
+    //const points = [{value:40, date:1420833600 },{value:80, date:1430424000},{value:60, date:1437681600},{value:54, date:1437681600}];
+    const points = [{value:40, date:1430424000 },{value:80, date:1437681600},{value:60, date:1443124800}];
+    const startDate = 1420056000;
+    const endDate = 1451505600;
+    const maxValue = 86;
     return (
       <div className="App">
-      <svg width="860" height="280">
-
-      <line fill="none" stroke="#e6e7e9" x1="35" y1="225" x2="840" y2="225" strokeWidth="1" opacity="1" />
-      <line fill="none" stroke="#e6e7e9" x1="35" y1="174" x2="840" y2="174" strokeWidth="1" opacity="1" />
-      <line fill="none" stroke="#e6e7e9" x1="35" y1="123" x2="840" y2="123" strokeWidth="1" opacity="1" />
-      <line fill="none" stroke="#e6e7e9" x1="35" y1="72" x2="840" y2="72" strokeWidth="1" opacity="1" />
-      <line fill="none" stroke="#e6e7e9" x1="35" y1="21" x2="840" y2="21" strokeWidth="1" opacity="1" />
-
-      <path fill="none" stroke="#78a1c1" d="M35,225L453,130L840,225" strokeWidth="1" opacity="1"></path>
-      <line fill="none" stroke="#d7d7d7" x1="453" y1="130" x2="453" y2="225" strokeDasharray="4,4" strokeWidth="1" opacity="1" />
-      <circle cx="453" cy="130" r="4" fill="#78a1c1" stroke="#f6f7f9" strokeWidth="2" opacity="1"></circle>
-
-      <text textAnchor="end" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
-        <tspan x="18" y="225">0</tspan>
-        <tspan x="18" y="174">20</tspan >
-        <tspan x="18" y="123">40</tspan >
-        <tspan x="18" y="72">60</tspan >
-        <tspan x="18" y="21">80</tspan>
-      </text>
-
-      <text textAnchor="middle" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
-        <tspan x="69" y="245">январь</tspan>
-        <tspan x="138" y="245">февраль</tspan>
-        <tspan x="207" y="245">март</tspan>
-        <tspan x="276" y="245">апрель</tspan >
-        <tspan x="345" y="245">май</tspan>
-        <tspan x="414" y="245">июнь</tspan>
-        <tspan x="473" y="245">июль</tspan>
-        <tspan x="542" y="245">август</tspan>
-        <tspan x="611" y="245">сентябрь</tspan>
-        <tspan x="670" y="245">октябрь</tspan>
-        <tspan x="739" y="245">ноябрь</tspan>
-        <tspan x="808" y="245">декабрь</tspan>
-      </text>
-
-      <text textAnchor="start" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
-        <tspan x="50" y="267">2015</tspan>
-      </text>
-
-  </svg>
+        <Chart points={points} startDate={startDate} endDate={endDate} maxValue={maxValue}/>
       </div>
     );
   }
--- src/Chart.js
+++ src/Chart.js
@@ -1,52 +1,144 @@
 import React, { Component } from 'react';
 
 class Chart extends Component {
-  render() {
-    return (
-      <svg width="860" height="280">
-      <g>
-        <line fill="none" stroke="#e6e7e9" x1="35" y1="225" x2="840" y2="225" strokeWidth="1" opacity="1" />
-        <line fill="none" stroke="#e6e7e9" x1="35" y1="174" x2="840" y2="174" strokeWidth="1" opacity="1" />
-        <line fill="none" stroke="#e6e7e9" x1="35" y1="123" x2="840" y2="123" strokeWidth="1" opacity="1" />
-        <line fill="none" stroke="#e6e7e9" x1="35" y1="72" x2="840" y2="72" strokeWidth="1" opacity="1" />
-        <line fill="none" stroke="#e6e7e9" x1="35" y1="21" x2="840" y2="21" strokeWidth="1" opacity="1" />
+  constructor(props) {
+   super(props);
+   this.state = {
+     tooltipTrigger: null,
+     currentPoint: {},
+     currentYears: '',
+     labelBottomOffset: 55,
+     labelLeftOffset: 35,
+     RightOffset: 20
+   }
+ }
 
+  graphComponent(points, start, end, maxValue) {
+    let path ='';
+    let dateRange = end - start;
+    let width = 860 - this.state.labelLeftOffset - this.state.RightOffset;
+    let height = 280 - this.state.labelBottomOffset;
+    let max = (maxValue + maxValue*0.1);
+    points.map((point, index) => {
+      let x = Math.round((point['date']-start) * width / dateRange);
+      console.log(x)
+      let y = height - Math.round(point['value'] * height / max);
+      path+=((index ? 'L' : 'M') + x + ',' + y);
+    });
+    return <path fill="none" stroke="#78a1c1" d={path} strokeWidth="1" opacity="1"></path>
+  }
+
+
+  axisX(start, end) {
+    const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    let labels = [];
+    let startDate = new Date(start*1000);
+    let endDate = new Date(end*1000);
+
+    let startYear = startDate.getYear();
+    let endYear = endDate.getYear();
+    let years = endYear - startYear;
+
+    let startMonth = startDate.getMonth();
+    let endMonth = endDate.getMonth();
+    let months = !years ? endMonth - startMonth + 1 : endMonth + (years * 12) - startMonth;
+
+    let width = 860 - this.state.labelLeftOffset - this.state.RightOffset;
+    let spaceXpx = width / months;
+
+    for (let i = 0; i < months; i++) {
+      labels.push(
+        <tspan x={this.state.labelLeftOffset + spaceXpx * i + spaceXpx / 2} y="245">{month[startDate.getMonth()]}</tspan>
+      );
+      startDate.setMonth(startDate.getMonth() + 1);
+    }
+
+    return <text textAnchor="middle" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
+        {labels}
+      </text>
+  }
+
+  axisY(maxValue) {
+    let labels = [];
+    let lines = [];
+    let max = Math.round((maxValue + maxValue*0.1));
+    let spaceY = max/4;
+    spaceY = Math.floor(spaceY/10)*10;
+    let start = 280 - this.state.labelBottomOffset;
+    let spaceYpx = spaceY*start/max;
+    for (let i = 0; i < 5; i++) {
+      let y = Math.round(start-spaceYpx*i);
+      lines.push(
+        <line fill="none" stroke="#e6e7e9" x1="35" y1={y} x2="840" y2={y} strokeWidth="1" opacity="1" />
+      );
+      labels.push(
+        <tspan key={i} x="18" y={y} >{spaceY*i}</tspan>
+      );
+    }
+    return (
+      <g>
+        {lines}
         <text textAnchor="end" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
-          <tspan x="18" y="225">0</tspan>
-          <tspan x="18" y="174">20</tspan >
-          <tspan x="18" y="123">40</tspan >
-          <tspan x="18" y="72">60</tspan >
-          <tspan x="18" y="21">80</tspan>
+          {labels}
         </text>
       </g>
-      <path fill="none" stroke="#78a1c1" d="M35,225L453,130L840,225" strokeWidth="1" opacity="1"></path>
-      <line fill="none" stroke="#d7d7d7" x1="453" y1="130" x2="453" y2="225" strokeDasharray="4,4" strokeWidth="1" opacity="1" />
-      <circle cx="453" cy="130" r="4" fill="#78a1c1" stroke="#f6f7f9" strokeWidth="2" opacity="1"></circle>
+    );
+  }
 
+  handleHover(trigger) {
+    console.log(trigger);
+    console.log('sdfsdf')
+    this.setState({
+      tooltipTrigger: trigger,
+    })
+  }
 
+  handleLeave() {
+    this.setState({
+      tooltipTrigger: null,
+    })
+  }
 
-      <text textAnchor="middle" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
-        <tspan x="69" y="245">январь</tspan>
-        <tspan x="138" y="245">февраль</tspan>
-        <tspan x="207" y="245">март</tspan>
-        <tspan x="276" y="245">апрель</tspan >
-        <tspan x="345" y="245">май</tspan>
-        <tspan x="414" y="245">июнь</tspan>
-        <tspan x="473" y="245">июль</tspan>
-        <tspan x="542" y="245">август</tspan>
-        <tspan x="611" y="245">сентябрь</tspan>
-        <tspan x="670" y="245">октябрь</tspan>
-        <tspan x="739" y="245">ноябрь</tspan>
-        <tspan x="808" y="245">декабрь</tspan>
+  numberYears(start, end) {
+    let startYear = new Date(start * 1000).getFullYear();
+    let endYear = new Date(end * 1000).getFullYear();
+    let years = endYear - startYear;
+
+    if (years) {
+      return <text textAnchor="start" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
+        <tspan x="50" y="267">{startYear} - {endYear}</tspan>
       </text>
-
-      <text textAnchor="start" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
-        <tspan x="50" y="267">2015</tspan>
+    } else {
+      return <text textAnchor="start" stroke="none" fill="#9d9ea2" opacity="1" strokeWidth="0">
+        <tspan x="50" y="267">{startYear}</tspan>
       </text>
+    }
+  }
 
-  </svg>
+  render() {
+    return (
+      <div className="chart">
+      <svg width="860" height="280">
+        {this.axisY(this.props.maxValue)}
+        {this.axisX(this.props.startDate, this.props.endDate)}
+        {this.numberYears(this.props.startDate, this.props.endDate)}
+
+        {this.graphComponent(this.props.points, this.props.startDate, this.props.endDate, this.props.maxValue)}
+
+        { this.props.points.map((point, i)=>{
+          return <rect onMouseOver={this.handleHover.bind(this,point)}
+          onMouseLeave={this.handleLeave.bind(this)}
+          key={i} x={point['date']} y={point['value']} width="10" height="100" fill="rgb(0,0,255)" stroke="red" strokeWidth="3" opacity="0.1"/>
+        })
+        }
+
+        {this.state.tooltipTrigger ? <circle cx={this.state.tooltipTrigger.date} cy={this.state.tooltipTrigger.value} r="4" fill="#78a1c1" stroke="#f6f7f9" strokeWidth="2" opacity="1"></circle> : null}
+        {this.state.tooltipTrigger ? <line fill="none" stroke="#d7d7d7" x1={this.state.tooltipTrigger.date} y1={this.state.tooltipTrigger.value} x2="453" y2="225" strokeDasharray="4,4" strokeWidth="1" opacity="1" /> : null}
+      </svg>
+      {this.state.tooltipTrigger ? <div className='tooltip'>value:{this.state.tooltipTrigger.value}, date:{this.state.tooltipTrigger.date}</div> : null}
+      </div>
     );
   }
 }
 
-export default App;
+export default Chart;
